1ea3beda8c04caac79014867764c43734a75fc7d
V <schindler.vanessa@outlook.de>
2020-02-13 16:04:20 +0100

    Flask Website start

---
+++ app/translate.py
@@ -0,0 +1,18 @@
+import json
+import requests
+from flask_babel import _
+from flask import current_app
+
+
+def translate(text, source_language, dest_language):
+    if 'MS_TRANSLATOR_KEY' not in current_app.config or \
+            not current_app.config['MS_TRANSLATOR_KEY']:
+        return 'Error: translation service not configured'
+    auth = {'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY']}
+    r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc'
+                     '/Translate?text={}&from={}&to={}'.format(
+                         text, source_language, dest_language),
+                     headers=auth)
+    if r.status_code != 200:
+        return 'Error'
+    return json.loads(r.content.decode('utf-8-sig'))
---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'test'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+    'sqlite:///' + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATION = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['vanessa.schindler@yahoo.de']
+    POSTS_PER_PAGE = 3
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
